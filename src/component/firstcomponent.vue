027e059631a11d1b532a28aeb8c7e7702fb4412e
Zero7Cc <zerocc7@163.com>
2018-03-29 19:56:34 +0800

    Initial vue project first

---
+++ src/component/firstcomponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="firstconponent">
+    <h1>我是标题</h1>
+    <a>是{{author}}写的</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        author:"zerocc"
+      }
+    }
+  }
+</script>
---
+++ src/component/secondcomponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="secondcomponent">
+    <h1>I am another page</h1>
+    <ul>
+      <li v-for="article in articles">
+        {{article.title}}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return{
+        author:"zero7CC",
+        articles: [],
+
+      }
+    },
+    mounted: function() {
+      this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+        headers: {
+
+        },
+        emulateJSON: true
+      }).then(function(response) {
+        // 这里是处理正确的回调
+
+        this.articles = response.data.subjects
+        // this.articles = response.data["subjects"] 也可以
+
+      }, function(response) {
+        // 这里是处理错误的回调
+        console.log(response)
+      });
+    }
+  }
+</script>
